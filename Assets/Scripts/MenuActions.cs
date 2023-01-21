0e6dea580e2f48ddd1d74483f4b8b91aa41a44e7
peucampos <peucampos@gmail.com>
2023-01-21 06:28:40 -0300

    Initial with Menu

---
+++ Assets/Scripts/MenuActions.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+    public void PlayBtn(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitBtn(){
+        Application.Quit();
+    }
+}
